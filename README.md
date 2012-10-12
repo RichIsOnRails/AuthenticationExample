f22607b26653d0b6aa966473ac002d40a479e8f8
Rich <rich@richonrails.com>
2012-10-11 23:11:51 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 AuthenticationExample
 =====================
 
-Authentication Example APp
+Authentication example app.  See <a href="http://www.richonrails.com/22012/10/12/authentication-in-rails-3-1">http://www.richonrails.com/2012/10/12/authentication-in-rails-3-1</a> for more info.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthenticationExample::Application.config.secret_token = '2677d0501c5dcfb8678f876a9a34ae4adcb710811affffc1bbcfe1d2b53ce9fc942be44698cf4c63041dd592bcc0ad0a417e0e2f3b6841d4acbbaf22351da963'
